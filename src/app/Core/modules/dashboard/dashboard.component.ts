0d390132f3c36ae75a78cc71d981225077f697f7
Arif Matubber <am@hoxro.com>
2021-07-20 06:15:56 +0600

    'k'

--- src/app/Core/modules/dashboard/dashboard.component.ts
+++ src/app/Core/modules/dashboard/dashboard.component.ts
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  isMorInforToggle = false;
 
-  constructor() { }
+  todaysSale = 125530;
+  totalSale = 12000225;
+  todaysPurchase = 823530;
+  totalPurchase = 53550225;
+
+    constructor() { }
 
   reorders: any[] = [{
     id: 1,
@@ -409,11 +415,616 @@ export class DashboardComponent implements OnInit {
     id: 100,
     prodName: "Tarts Assorted",
     prodQty: "36"
-  }]
+  }];
 
+  activeOrders: any[] = [{
+    id: 1,
+    orderNumber: "09-903-0846",
+    customer: "Alisha Clute",
+    date: "2/17/2021",
+    status: false
+  }, {
+    id: 2,
+    orderNumber: "43-579-3915",
+    customer: "Emmy Christescu",
+    date: "9/5/2020",
+    status: false
+  }, {
+    id: 3,
+    orderNumber: "95-810-4438",
+    customer: "Cosetta Paulack",
+    date: "4/18/2021",
+    status: true
+  }, {
+    id: 4,
+    orderNumber: "87-571-8992",
+    customer: "Jarret Haugen",
+    date: "4/22/2021",
+    status: false
+  }, {
+    id: 5,
+    orderNumber: "87-661-8770",
+    customer: "Julee Innot",
+    date: "5/1/2021",
+    status: false
+  }, {
+    id: 6,
+    orderNumber: "40-849-7355",
+    customer: "Bentlee Hennington",
+    date: "9/14/2020",
+    status: false
+  }, {
+    id: 7,
+    orderNumber: "53-298-3693",
+    customer: "Bartholomeo McDunlevy",
+    date: "6/26/2021",
+    status: false
+  }, {
+    id: 8,
+    orderNumber: "46-090-8323",
+    customer: "Alain Michal",
+    date: "9/23/2020",
+    status: true
+  }, {
+    id: 9,
+    orderNumber: "71-496-7354",
+    customer: "Haily Trulock",
+    date: "8/22/2020",
+    status: true
+  }, {
+    id: 10,
+    orderNumber: "68-125-3610",
+    customer: "Carmine Chavez",
+    date: "9/5/2020",
+    status: true
+  }, {
+    id: 11,
+    orderNumber: "56-743-4205",
+    customer: "Bevin Frisel",
+    date: "2/8/2021",
+    status: false
+  }, {
+    id: 12,
+    orderNumber: "25-529-0483",
+    customer: "Sophie Ferrarin",
+    date: "1/5/2021",
+    status: true
+  }, {
+    id: 13,
+    orderNumber: "73-647-1452",
+    customer: "Vincent Royste",
+    date: "10/3/2020",
+    status: false
+  }, {
+    id: 14,
+    orderNumber: "35-544-6333",
+    customer: "Lorry Duns",
+    date: "9/13/2020",
+    status: true
+  }, {
+    id: 15,
+    orderNumber: "90-528-9562",
+    customer: "Ruggiero Baum",
+    date: "9/29/2020",
+    status: true
+  }, {
+    id: 16,
+    orderNumber: "93-555-1699",
+    customer: "Ozzie Ruggiero",
+    date: "9/22/2020",
+    status: true
+  }, {
+    id: 17,
+    orderNumber: "61-112-5098",
+    customer: "Jarib Domenc",
+    date: "1/5/2021",
+    status: false
+  }, {
+    id: 18,
+    orderNumber: "41-507-9859",
+    customer: "Nelli Elcott",
+    date: "6/16/2021",
+    status: true
+  }, {
+    id: 19,
+    orderNumber: "54-126-3981",
+    customer: "Pier Clampe",
+    date: "11/18/2020",
+    status: false
+  }, {
+    id: 20,
+    orderNumber: "04-438-4273",
+    customer: "Cary Betham",
+    date: "3/31/2021",
+    status: true
+  }, {
+    id: 21,
+    orderNumber: "35-755-5680",
+    customer: "Zsa zsa Provost",
+    date: "12/22/2020",
+    status: true
+  }, {
+    id: 22,
+    orderNumber: "13-085-8312",
+    customer: "Matty Kaaskooper",
+    date: "9/21/2020",
+    status: false
+  }, {
+    id: 23,
+    orderNumber: "18-033-6883",
+    customer: "Lela Bickerdike",
+    date: "6/18/2021",
+    status: true
+  }, {
+    id: 24,
+    orderNumber: "78-792-3638",
+    customer: "Tommie Gwalter",
+    date: "6/29/2021",
+    status: true
+  }, {
+    id: 25,
+    orderNumber: "66-713-4179",
+    customer: "Nealson Womack",
+    date: "4/28/2021",
+    status: false
+  }, {
+    id: 26,
+    orderNumber: "74-236-2317",
+    customer: "Steven Rosenfeld",
+    date: "1/15/2021",
+    status: true
+  }, {
+    id: 27,
+    orderNumber: "25-190-7799",
+    customer: "Tandie Oxbe",
+    date: "7/27/2020",
+    status: true
+  }, {
+    id: 28,
+    orderNumber: "61-884-1851",
+    customer: "Alard Sparhawk",
+    date: "4/1/2021",
+    status: false
+  }, {
+    id: 29,
+    orderNumber: "20-957-9642",
+    customer: "Claudio Dymock",
+    date: "6/18/2021",
+    status: false
+  }, {
+    id: 30,
+    orderNumber: "62-054-0708",
+    customer: "Nicolle Lendon",
+    date: "8/21/2020",
+    status: false
+  }, {
+    id: 31,
+    orderNumber: "50-786-1199",
+    customer: "Thaine Volkers",
+    date: "11/30/2020",
+    status: true
+  }, {
+    id: 32,
+    orderNumber: "59-010-8667",
+    customer: "Milzie Prine",
+    date: "3/14/2021",
+    status: false
+  }, {
+    id: 33,
+    orderNumber: "77-236-1627",
+    customer: "Windham Rowlands",
+    date: "8/27/2020",
+    status: false
+  }, {
+    id: 34,
+    orderNumber: "02-244-5963",
+    customer: "Wyn Goldthorp",
+    date: "11/27/2020",
+    status: true
+  }, {
+    id: 35,
+    orderNumber: "33-818-8556",
+    customer: "Daron Vivyan",
+    date: "11/23/2020",
+    status: false
+  }, {
+    id: 36,
+    orderNumber: "83-879-5956",
+    customer: "Roseann Hampstead",
+    date: "4/25/2021",
+    status: true
+  }, {
+    id: 37,
+    orderNumber: "28-506-7474",
+    customer: "Gillan Stringer",
+    date: "2/15/2021",
+    status: false
+  }, {
+    id: 38,
+    orderNumber: "61-925-3253",
+    customer: "Kandace Wilfing",
+    date: "10/15/2020",
+    status: false
+  }, {
+    id: 39,
+    orderNumber: "11-616-9514",
+    customer: "Arvin Kubicki",
+    date: "10/23/2020",
+    status: true
+  }, {
+    id: 40,
+    orderNumber: "25-389-9045",
+    customer: "Corey Losano",
+    date: "7/2/2021",
+    status: true
+  }, {
+    id: 41,
+    orderNumber: "72-216-9672",
+    customer: "Mirabel Boyen",
+    date: "8/4/2020",
+    status: true
+  }, {
+    id: 42,
+    orderNumber: "86-915-5581",
+    customer: "Eulalie Pinnell",
+    date: "12/4/2020",
+    status: false
+  }, {
+    id: 43,
+    orderNumber: "40-171-4565",
+    customer: "Hailee Crummey",
+    date: "6/20/2021",
+    status: false
+  }, {
+    id: 44,
+    orderNumber: "20-067-4714",
+    customer: "Wallie Moulster",
+    date: "10/13/2020",
+    status: false
+  }, {
+    id: 45,
+    orderNumber: "65-815-4903",
+    customer: "Bendick Ridgedell",
+    date: "12/29/2020",
+    status: false
+  }, {
+    id: 46,
+    orderNumber: "03-222-9251",
+    customer: "Fionnula Traske",
+    date: "5/7/2021",
+    status: true
+  }, {
+    id: 47,
+    orderNumber: "59-778-1763",
+    customer: "Padraig Wormald",
+    date: "11/24/2020",
+    status: false
+  }, {
+    id: 48,
+    orderNumber: "16-852-1690",
+    customer: "Josi O' Meara",
+    date: "12/11/2020",
+    status: true
+  }, {
+    id: 49,
+    orderNumber: "30-127-5869",
+    customer: "Allis Cullip",
+    date: "12/7/2020",
+    status: true
+  }, {
+    id: 50,
+    orderNumber: "90-813-9012",
+    customer: "Cassius Speers",
+    date: "2/22/2021",
+    status: true
+  }, {
+    id: 51,
+    orderNumber: "06-713-4080",
+    customer: "Damita Waudby",
+    date: "8/28/2020",
+    status: false
+  }, {
+    id: 52,
+    orderNumber: "55-049-8292",
+    customer: "Robinson Janusik",
+    date: "10/3/2020",
+    status: true
+  }, {
+    id: 53,
+    orderNumber: "59-089-6501",
+    customer: "Cornall MacKeever",
+    date: "10/7/2020",
+    status: true
+  }, {
+    id: 54,
+    orderNumber: "30-309-1179",
+    customer: "Diane Disbrey",
+    date: "12/12/2020",
+    status: true
+  }, {
+    id: 55,
+    orderNumber: "97-371-5191",
+    customer: "Dacey Elsmere",
+    date: "11/16/2020",
+    status: true
+  }, {
+    id: 56,
+    orderNumber: "42-044-7875",
+    customer: "Lauraine Cobden",
+    date: "9/19/2020",
+    status: false
+  }, {
+    id: 57,
+    orderNumber: "42-359-7135",
+    customer: "Mellie Lalevee",
+    date: "3/30/2021",
+    status: true
+  }, {
+    id: 58,
+    orderNumber: "67-843-7040",
+    customer: "Stanly Zanitti",
+    date: "3/22/2021",
+    status: false
+  }, {
+    id: 59,
+    orderNumber: "10-035-8727",
+    customer: "Dena Westphalen",
+    date: "11/8/2020",
+    status: false
+  }, {
+    id: 60,
+    orderNumber: "16-775-3312",
+    customer: "Gilberte Meysham",
+    date: "1/31/2021",
+    status: true
+  }, {
+    id: 61,
+    orderNumber: "10-201-2021",
+    customer: "Isabelle Ayscough",
+    date: "10/10/2020",
+    status: false
+  }, {
+    id: 62,
+    orderNumber: "98-133-2930",
+    customer: "Colet Casetta",
+    date: "2/28/2021",
+    status: true
+  }, {
+    id: 63,
+    orderNumber: "58-394-6968",
+    customer: "Major McKerley",
+    date: "7/11/2021",
+    status: true
+  }, {
+    id: 64,
+    orderNumber: "83-294-8247",
+    customer: "Jamal Tregido",
+    date: "12/19/2020",
+    status: false
+  }, {
+    id: 65,
+    orderNumber: "72-319-9154",
+    customer: "Garold Jan",
+    date: "11/20/2020",
+    status: true
+  }, {
+    id: 66,
+    orderNumber: "84-102-5177",
+    customer: "Constantine Gianolo",
+    date: "1/25/2021",
+    status: true
+  }, {
+    id: 67,
+    orderNumber: "42-102-5538",
+    customer: "Fraser Marians",
+    date: "6/28/2021",
+    status: true
+  }, {
+    id: 68,
+    orderNumber: "42-658-4449",
+    customer: "Oates Batalini",
+    date: "2/12/2021",
+    status: false
+  }, {
+    id: 69,
+    orderNumber: "63-319-4550",
+    customer: "Albertine Watson",
+    date: "6/9/2021",
+    status: false
+  }, {
+    id: 70,
+    orderNumber: "16-072-6063",
+    customer: "Maryellen Bergin",
+    date: "6/3/2021",
+    status: false
+  }, {
+    id: 71,
+    orderNumber: "80-062-0014",
+    customer: "Llywellyn Stoppard",
+    date: "5/23/2021",
+    status: true
+  }, {
+    id: 72,
+    orderNumber: "05-938-1858",
+    customer: "Buffy Sangwine",
+    date: "7/3/2021",
+    status: true
+  }, {
+    id: 73,
+    orderNumber: "55-860-3570",
+    customer: "Judith Whithorn",
+    date: "8/9/2020",
+    status: true
+  }, {
+    id: 74,
+    orderNumber: "25-825-6928",
+    customer: "Royal Pirouet",
+    date: "4/17/2021",
+    status: false
+  }, {
+    id: 75,
+    orderNumber: "99-422-2673",
+    customer: "Tristam Gath",
+    date: "4/30/2021",
+    status: false
+  }, {
+    id: 76,
+    orderNumber: "64-796-5485",
+    customer: "Albina Von Salzberg",
+    date: "5/23/2021",
+    status: false
+  }, {
+    id: 77,
+    orderNumber: "60-569-0556",
+    customer: "Rowney Lording",
+    date: "8/9/2020",
+    status: true
+  }, {
+    id: 78,
+    orderNumber: "28-336-7394",
+    customer: "Ardene Lebarree",
+    date: "8/20/2020",
+    status: false
+  }, {
+    id: 79,
+    orderNumber: "63-568-4897",
+    customer: "Michale Curtayne",
+    date: "10/5/2020",
+    status: true
+  }, {
+    id: 80,
+    orderNumber: "01-495-9642",
+    customer: "Ema Calvard",
+    date: "9/9/2020",
+    status: true
+  }, {
+    id: 81,
+    orderNumber: "40-847-8829",
+    customer: "Cliff Croxall",
+    date: "1/18/2021",
+    status: true
+  }, {
+    id: 82,
+    orderNumber: "08-203-2982",
+    customer: "Aida Giacomuzzi",
+    date: "2/4/2021",
+    status: false
+  }, {
+    id: 83,
+    orderNumber: "06-398-9144",
+    customer: "Anne-marie Hagergham",
+    date: "2/11/2021",
+    status: false
+  }, {
+    id: 84,
+    orderNumber: "62-450-6797",
+    customer: "Rhona Whitland",
+    date: "9/3/2020",
+    status: false
+  }, {
+    id: 85,
+    orderNumber: "48-416-8795",
+    customer: "Megen Dugan",
+    date: "3/11/2021",
+    status: false
+  }, {
+    id: 86,
+    orderNumber: "60-160-3115",
+    customer: "Aldridge Rawling",
+    date: "7/29/2020",
+    status: true
+  }, {
+    id: 87,
+    orderNumber: "38-694-1085",
+    customer: "Skipton Trainor",
+    date: "5/10/2021",
+    status: false
+  }, {
+    id: 88,
+    orderNumber: "67-794-2402",
+    customer: "Salome Gaydon",
+    date: "8/28/2020",
+    status: false
+  }, {
+    id: 89,
+    orderNumber: "40-915-1961",
+    customer: "Mabel Voyce",
+    date: "3/9/2021",
+    status: true
+  }, {
+    id: 90,
+    orderNumber: "03-760-6289",
+    customer: "Tremayne MacAdie",
+    date: "6/9/2021",
+    status: true
+  }, {
+    id: 91,
+    orderNumber: "04-305-1458",
+    customer: "Tamera Episcopi",
+    date: "10/11/2020",
+    status: false
+  }, {
+    id: 92,
+    orderNumber: "64-074-5580",
+    customer: "Brendon Simkovich",
+    date: "12/19/2020",
+    status: false
+  }, {
+    id: 93,
+    orderNumber: "11-568-6932",
+    customer: "Martelle Belvin",
+    date: "4/7/2021",
+    status: false
+  }, {
+    id: 94,
+    orderNumber: "86-846-6494",
+    customer: "Matthaeus Rapley",
+    date: "5/26/2021",
+    status: true
+  }, {
+    id: 95,
+    orderNumber: "46-281-4031",
+    customer: "Dorelia Maginn",
+    date: "12/16/2020",
+    status: true
+  }, {
+    id: 96,
+    orderNumber: "42-662-4880",
+    customer: "Janet Mansion",
+    date: "7/31/2020",
+    status: true
+  }, {
+    id: 97,
+    orderNumber: "95-174-7549",
+    customer: "Conrade Whiteson",
+    date: "1/16/2021",
+    status: false
+  }, {
+    id: 98,
+    orderNumber: "26-874-7214",
+    customer: "Ginny Pharaoh",
+    date: "9/14/2020",
+    status: false
+  }, {
+    id: 99,
+    orderNumber: "67-612-8073",
+    customer: "Staci Esposita",
+    date: "7/15/2021",
+    status: true
+  }, {
+    id: 100,
+    orderNumber: "72-945-7304",
+    customer: "Carlyn Deener",
+    date: "1/14/2021",
+    status: false
+  }];
+  
   ngOnInit(): void {
   }
 
+  toggleMoreInfo(){
+this.isMorInforToggle =! this.isMorInforToggle;
+  }
 
 
 }
--- src/app/Core/modules/products/components/product-list/product-list.component.ts
+++ src/app/Core/modules/products/components/product-list/product-list.component.ts
@@ -12,4 +12,553 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  products: any[] = [
+    {
+        
+        id: 577,
+        productCode: "bts300",
+        productName: "Alin baby toast 300gm",
+        description: "12pcs per Carton",
+        cost: 527.16,
+        price: 527.16,
+        reoder: 20,
+        qty: 12,
+        active: false,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 578,
+        productCode: "bcm14",
+        productName: "Alin Box cake .milk 14gm",
+        description: "20pcs per Carton",
+        cost: 897.60,
+        price: 897.60,
+        reoder: 20,
+        qty: 20,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 579,
+        productCode: "cc13",
+        productName: "Alin choco cake foil pack 13gm",
+        description: "72pcs per Carton",
+        cost: 269.28,
+        price: 269.28,
+        reoder: 20,
+        qty: 72,
+        active: false,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 580,
+        productCode: "cp14",
+        productName: "Alin cup cake 14gm",
+        description: "72pcs per Carton",
+        cost: 269.28,
+        price: 269.28,
+        reoder: 20,
+        qty: 72,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 581,
+        productCode: "dc40",
+        productName: "Alin Dry cake  F/P 40gm",
+        description: "48pcs per Carton",
+        cost: 359.04,
+        price: 359.04,
+        reoder: 20,
+        qty: 48,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 582,
+        productCode: "fc14",
+        productName: "Alin fruitti cake f/p 14 gm",
+        description: "72pcs per Carton",
+        cost: 269.28,
+        price: 269.28,
+        reoder: 20,
+        qty: 72,
+        active: false,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 583,
+        productCode: "g300",
+        productName: "Alin ghee Rusk f/p 300gm",
+        description: "12pcs per Carton",
+        cost: 538.32,
+        price: 538.32,
+        reoder: 20,
+        qty: 12,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 584,
+        productCode: "stb50",
+        productName: "Alin salted biscuits 50gm",
+        description: "48pcs per Carton",
+        cost: 362.40,
+        price: 362.40,
+        reoder: 20,
+        qty: 48,
+        active: false,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 585,
+        productCode: "sr20",
+        productName: "Alin sweet rusk 20 gm",
+        description: "48pcs per Carton",
+        cost: 179.52,
+        price: 179.52,
+        reoder: 20,
+        qty: 48,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 586,
+        productCode: "ts24",
+        productName: "Alin Tea toast 24 gm",
+        description: "48pcs per Carton",
+        cost: 179.52,
+        price: 179.52,
+        reoder: 20,
+        qty: 48,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 587,
+        productCode: "st300",
+        productName: "Alin toast 300gm",
+        description: "6pcs per Carton",
+        cost: 224.28,
+        price: 224.28,
+        reoder: 20,
+        qty: 6,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 588,
+        productCode: "m1000",
+        productName: "Alin Mango  Drinks 1000 L/t",
+        description: "12pcs per Carton",
+        cost: 684.72,
+        price: 684.72,
+        reoder: 20,
+        qty: 12,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 589,
+        productCode: "m250",
+        productName: "Mango Drinks  250 ml",
+        description: "24pcs per Carton",
+        cost: 414.72,
+        price: 414.72,
+        reoder: 20,
+        qty: 24,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 590,
+        productCode: "m500",
+        productName: "Mango drinks 500 ml",
+        description: "24pcs per Carton",
+        cost: 768.24,
+        price: 768.24,
+        reoder: 20,
+        qty: 24,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 591,
+        productCode: "l250",
+        productName: "Litchi Drinks 250 ml",
+        description: "24pcs per Carton",
+        cost: 269.04,
+        price: 269.04,
+        reoder: 20,
+        qty: 24,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 592,
+        productCode: "ol200",
+        productName: "Alin oil 200 ml",
+        description: "24pcs per Carton",
+        cost: 986.88,
+        price: 986.88,
+        reoder: 20,
+        qty: 24,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 593,
+        productCode: "ol500",
+        productName: "Alin Oil 500 ml",
+        description: "24pcs per Carton",
+        cost: 213096,
+        price: 213096,
+        reoder: 20,
+        qty: 24,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 594,
+        productCode: "ol80",
+        productName: "Alin Oil 80 ml",
+        description: "48pcs per Carton",
+        cost: 114300,
+        price: 114300,
+        reoder: 20,
+        qty: 48,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 595,
+        productCode: "ts10",
+        productName: "Alin tomato sauce 10gm",
+        description: "48pcs per Carton",
+        cost: 944.64,
+        price: 944.64,
+        reoder: 20,
+        qty: 48,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 596,
+        productCode: "pr400",
+        productName: "Alin puffed rice 400gm",
+        description: "24pcs per Carton",
+        cost: 785.04,
+        price: 785.04,
+        reoder: 20,
+        qty: 24,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 597,
+        productCode: "ls200",
+        productName: "Alin Lascha shemai 200gm",
+        description: "25pcs per Carton",
+        cost: 588.75,
+        price: 588.75,
+        reoder: 20,
+        qty: 25,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 598,
+        productCode: "n180",
+        productName: "Stick Noodles 180 gm",
+        description: "24pcs per Carton",
+        cost: 336.48,
+        price: 336.48,
+        reoder: 20,
+        qty: 24,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 599,
+        productCode: "n80",
+        productName: "stick Noodles 80 gm",
+        description: "36pcs per Carton",
+        cost: 269.28,
+        price: 269.28,
+        reoder: 20,
+        qty: 36,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 600,
+        productCode: "vs200",
+        productName: "Alin Vermicelli  200gm",
+        description: "24pcs per Carton",
+        cost: 516.00,
+        price: 516.00,
+        reoder: 20,
+        qty: 24,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 602,
+        productCode: "fp20",
+        productName: "Alin bbq fried peas 20gm",
+        description: "240pcs per Carton",
+        cost: 897.60,
+        price: 897.60,
+        reoder: 20,
+        qty: 240,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 603,
+        productCode: "cb18",
+        productName: "Alin chira bujia 18gm",
+        description: "240pcs per Carton",
+        cost: 897.60,
+        price: 897.60,
+        reoder: 20,
+        qty: 240,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 604,
+        productCode: "ffd15",
+        productName: "Alin fried dal 15gm",
+        description: "240pcs per Carton",
+        cost: 897.60,
+        price: 897.60,
+        reoder: 20,
+        qty: 240,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 605,
+        productCode: "ffp",
+        productName: "Alin fried peas 20 gm",
+        description: "240pcs per Carton",
+        cost: 897.60,
+        price: 897.60,
+        reoder: 20,
+        qty: 240,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 606,
+        productCode: "ch20",
+        productName: "Alin chanachur 20 gm",
+        description: "240pcs per Carton",
+        cost: 897.60,
+        price: 897.60,
+        reoder: 20,
+        qty: 240,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 607,
+        productCode: "ch300",
+        productName: "Alin chanachur 300 gm",
+        description: "24pcs per Carton",
+        cost: 986.88,
+        price: 986.88,
+        reoder: 20,
+        qty: 24,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 608,
+        productCode: "ppr35",
+        productName: "Alin puffed rice 35gm",
+        description: "240pcs per Carton",
+        cost: 147360,
+        price: 147360,
+        reoder: 20,
+        qty: 240,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 609,
+        productCode: "mch20",
+        productName: "Alin savoury mix chanachur 20gm",
+        description: "240pcs per Carton",
+        cost: 897.60,
+        price: 897.60,
+        reoder: 20,
+        qty: 240,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 610,
+        productCode: "bm40",
+        productName: "Alin Biryani masala 40 gm",
+        description: "24pcs per Carton",
+        cost: 886.08,
+        price: 886.08,
+        reoder: 20,
+        qty: 24,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 611,
+        productCode: "cm20",
+        productName: "Alin chiken curry powder 20gm",
+        description: "144pcs per Carton",
+        cost: 148032,
+        price: 148032,
+        reoder: 20,
+        qty: 144,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 612,
+        productCode: "cp50",
+        productName: "Alin chilli powder 50gm",
+        description: "96pcs per Carton",
+        cost: 179424,
+        price: 179424,
+        reoder: 20,
+        qty: 96,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 613,
+        productCode: "mc20",
+        productName: "Alin meat curry powder 20gm",
+        description: "144pcs per Carton",
+        cost: 148032,
+        price: 148032,
+        reoder: 20,
+        qty: 144,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 614,
+        productCode: "tp50",
+        productName: "Alin turmeric powder 50gm",
+        description: "96pcs per Carton",
+        cost: 179424,
+        price: 179424,
+        reoder: 20,
+        qty: 96,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 615,
+        productCode: "cm24",
+        productName: "Alin muffin cake cho 24gm",
+        description: "48pcs per Carton",
+        cost: 358.56,
+        price: 358.56,
+        reoder: 20,
+        qty: 48,
+        active: true,
+        category: "Food"
+        
+    },
+    {
+        
+        id: 616,
+        productCode: "ch150",
+        productName: "Alin chanachur 150 gm",
+        description: "48pcs per Carton",
+        cost: 996.00,
+        price: 996.00,
+        reoder: 20,
+        qty: 48,
+        active: true,
+        category: "Food"
+        
+    }
+];
+
 }
